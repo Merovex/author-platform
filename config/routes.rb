395f49d53527eafb2287820014a5d6f9c8cfd7be
Ben W <merovex@hey.com>
2021-09-29 11:42:57 -0400

    working devise Confirmation issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  
+  resources :confirmations
   resources :posts
   devise_for :users
+  get 'landing/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # Almost every application defines a route for the root path ("/") at the top of this file.
   # root "articles#index"
+  root to: 'landing#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -69,10 +69,24 @@ ActiveRecord::Schema.define(version: 2021_09_29_111908) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: 6
     t.datetime "remember_created_at", precision: 6
+    t.integer "sign_in_count", default: 0, null: false
+    t.datetime "current_sign_in_at", precision: 6
+    t.datetime "last_sign_in_at", precision: 6
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at", precision: 6
+    t.datetime "confirmation_sent_at", precision: 6
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at", precision: 6
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
