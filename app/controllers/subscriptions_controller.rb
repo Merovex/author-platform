756a47c41b55aee945e2f3f5645a17af3f121516
Ben W <merovex@hey.com>
2021-11-03 08:07:53 -0400

    refactor subscriptions_controller

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -2,74 +2,26 @@ class SubscriptionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_subscription, only: %i[ index show update destroy ]
 
-  # GET /subscriptions or /subscriptions.json
   def index
-    
     render :show
   end
   def subscribers
-    # @subscriptions = Subscription.all
-
+    @subscriptions = Subscription.all
   end
-
-  # GET /subscriptions/1 or /subscriptions/1.json
-  def show
-  end
-
-  # GET /subscriptions/new
-  def new
-    @subscription = Subscription.new
-  end
-
-  # GET /subscriptions/1/edit
-  def edit
-  end
-
-  # # POST /subscriptions or /subscriptions.json
-  # def create
-  #   @subscription = Subscription.new(subscription_params)
-
-  #   respond_to do |format|
-  #     if @subscription.save
-  #       format.html { redirect_to @subscription, notice: "Subscription was successfully created." }
-  #       format.json { render :show, status: :created, location: @subscription }
-  #     else
-  #       format.html { render :new, status: :unprocessable_entity }
-  #       format.json { render json: @subscription.errors, status: :unprocessable_entity }
-  #     end
-  #   end
-  # end
-
-  # PATCH/PUT /subscriptions/1 or /subscriptions/1.json
   def update
     [ %i[wants_posts posts], %i[wants_books books] ].each do |check, key|
-      wants   = (subscription_params[check].nil?) ? false : true
-      has_not = (@subscription.read_attribute(key).nil?) ? true : false
+      wants = (subscription_params[check].nil?) ? false : true
+      has   = (@subscription.read_attribute(key).nil?) ? false : true 
       
-      @subscription.unsubscribe(key) unless wants && !has_not
-      @subscription.subscribe(key) if wants && has_not
+      @subscription.unsubscribe(key) unless wants && has
+      @subscription.subscribe(key) if wants && !has
     end
-
+    @subscription.save
     respond_to do |format|
-      if @subscription.save
-        format.html { redirect_to @subscription, notice: "Subscription was successfully updated." }
-        format.json { render :show, status: :ok, location: @subscription }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @subscription.errors, status: :unprocessable_entity }
-      end
+      format.html { redirect_to subscriptions_url, notice: "Subscription was successfully updated." }
+      format.json { render subscriptions_url, status: :ok, location: @subscription }
     end
   end
-
-  # # DELETE /subscriptions/1 or /subscriptions/1.json
-  # def destroy
-  #   @subscription.destroy
-  #   respond_to do |format|
-  #     format.html { redirect_to subscriptions_url, notice: "Subscription was successfully destroyed." }
-  #     format.json { head :no_content }
-  #   end
-  # end
-
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_subscription
