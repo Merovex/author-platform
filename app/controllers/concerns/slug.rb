ec89d4ad6b1d2a991952ab47db4b929668e5e173
Ben W <merovex@hey.com>
2022-06-04 15:15:14 -0400

    problem with slug.

--- app/controllers/concerns/slug.rb
+++ app/controllers/concerns/slug.rb
@@ -10,6 +10,7 @@ module Slug
     def unique_slug(key)
       loop do
         slug = self.make_slug
+        return nil unless self.exists?
         return slug unless self.exists?({key.to_sym => slug})
       end
     end
