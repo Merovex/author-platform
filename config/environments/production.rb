ab459fd4c804419a59bc8f6126754e5c8402953c
Ben W <merovex@hey.com>
2022-06-05 16:06:25 -0400

    Fixin sendgrid.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings.smtp_settings = {
   #   user_name:      'apikey',
   #   password:       ENV['SENDGRID_PASSWORD'],
