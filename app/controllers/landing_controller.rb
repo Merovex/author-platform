7fdd4950719dd5d59cad260e2b7ddbae02783d4f
Ben W <merovex@hey.com>
2022-06-05 06:14:05 -0400

    Switching to Posts#index for no-book Landing page

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,9 +2,9 @@ class LandingController < ApplicationController
   def index
     @book = Book.featured.last || Book.last
     if @book.nil?
-      # @posts = Post.all
-      # render "posts/index"
-      render :index
+      @posts = Post.all
+      render "posts/index"
+      # render :index
     else
       render "books/show"
     end
