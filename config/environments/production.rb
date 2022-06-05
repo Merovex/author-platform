d6daa31212ad6e786c7f24ff606fb22942076cbd
Ben W <merovex@hey.com>
2022-06-05 12:02:42 -0400

    restoring slug.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
+  Rails.application.routes.default_url_options[:host] = "merovex-author-website.herokuapp.com"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
