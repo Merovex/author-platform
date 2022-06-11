33471bfb2ee21cab1290fac9e3a3cf1759818eb1
Ben W <merovex@hey.com>
2022-06-11 10:20:35 -0400

    Add clicks

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
   # You set your `config.assets.compile = true` in production.
   # This can negatively impact the performance of your application.
   # 
