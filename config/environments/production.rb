89a581b0fc84cb6c6a46948ec5bf2826bab1744b
Ben W <merovex@hey.com>
2022-06-11 09:46:37 -0400

    Adding missing file.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,12 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
-  config.assets.compile = true
+  config.assets.compile = false
+  # You set your `config.assets.compile = true` in production.
+  # This can negatively impact the performance of your application.
+  # 
+  # For more information can be found in this article:
+  #   https://devcenter.heroku.com/articles/rails-asset-pipeline#compile-set-to-true-in-production
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
