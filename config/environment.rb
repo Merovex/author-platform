fe3acc3ce8320ecb77157c9c3bf07b3c64eb2c3f
Ben W <merovex@hey.com>
2022-06-11 07:39:18 -0400

    Testing sendmail.

--- config/environment.rb
+++ config/environment.rb
@@ -4,14 +4,4 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-# ActionMailer::Base.smtp_settings = {
-#   user_name:      'apikey',
-#   password:       ENV['SENDGRID_PASSWORD'],
-#   # password:       Rails.application.credentials.dig(:sendgrid, :api_key),
-#   domain:         'merovex-author-website.herokuapp.com',
-#   address:        "smtp.sendgrid.net",
-#   port:            587,
-#   authentication: :plain,
-#   enable_starttls_auto: true
-# }
 #u = User.first; u.subscription.destroy; u.destroy; u = User.new(email: 'merovex@gmail.com').save
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,16 +66,17 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'apikey',
-    password:       ENV['SENDGRID_PASSWORD'],
-    domain:         'merovex-author-website.herokuapp.com',
-    address:        "smtp.sendgrid.net",
-    port:            587,
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
+  # config.action_mailer.delivery_method = :smtp
+  # Sendmail Configuration
+  # config.action_mailer.smtp_settings = {
+  #   user_name:      'apikey',
+  #   password:       ENV['SENDGRID_PASSWORD'],
+  #   domain:         'merovex-author-website.herokuapp.com',
+  #   address:        "smtp.sendgrid.net",
+  #   port:            587,
+  #   authentication: :plain,
+  #   enable_starttls_auto: true
+  # }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
