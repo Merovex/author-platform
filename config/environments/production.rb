5839965087d0dc9563b6cb94d946e4400374f48b
Ben W <merovex@hey.com>
2022-06-12 12:47:30 -0400

    fix mailer from error.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
   # You set your `config.assets.compile = true` in production.
   # This can negatively impact the performance of your application.
   # 
