a9e3428f58963fa2e64ad5840bad075af7a4592e
Ben W <merovex@hey.com>
2022-06-19 16:39:04 -0400

    Adding old fonts & fontstyle?

---
+++ app/assets/stylesheets/fonts/fonts.rb
@@ -0,0 +1,67 @@
+
+
+fonts = %w(FjallaOne-Regular.ttf
+SourceCodePro-Black.ttf
+SourceCodePro-BlackItalic.ttf
+SourceCodePro-Bold.ttf
+SourceCodePro-BoldItalic.ttf
+SourceCodePro-ExtraBold.ttf
+SourceCodePro-ExtraBoldItalic.ttf
+SourceCodePro-Italic.ttf
+SourceCodePro-Medium.ttf
+SourceCodePro-MediumItalic.ttf
+SourceCodePro-Regular.ttf
+SourceCodePro-SemiBold.ttf
+SourceCodePro-SemiBoldItalic.ttf
+SourceSansPro-Black.ttf
+SourceSansPro-BlackItalic.ttf
+SourceSansPro-Bold.ttf
+SourceSansPro-BoldItalic.ttf
+SourceSansPro-Italic.ttf
+SourceSansPro-Regular.ttf
+SourceSansPro-SemiBold.ttf
+SourceSansPro-SemiBoldItalic.ttf
+SourceSerifPro-Black.ttf
+SourceSerifPro-BlackItalic.ttf
+SourceSerifPro-Bold.ttf
+SourceSerifPro-BoldItalic.ttf
+SourceSerifPro-Italic.ttf
+SourceSerifPro-Regular.ttf
+SourceSerifPro-SemiBold.ttf
+SourceSerifPro-SemiBoldItalic.ttf)
+
+contents = fonts.map do |font|
+  weight = case true
+  when font.include?("SemiBold")
+    600
+  when font.include?("Bold")
+    800
+  when font.include?("Medium")
+    500
+  else 
+    400
+  end
+  type = case
+  when font.include?("Code")
+    "Code"
+  when font.include?("Sans")
+    "Sans"
+  when font.include?("FjallaOne")
+    "Display"
+  else
+    "Serif"
+  end
+  style = (font.include?("Italic")) ? "italic" : "normal";
+  template = <<-EOS
+@font-face {
+  font-family: 'Merovex #{type}';
+  font-display: swap;
+  font-weight: #{weight};
+  font-style: #{style};
+  font-stretch: normal;
+  src: url('./fonts/#{font}') format('truetype');
+}
+EOS
+  template
+end.join("\n")
+puts contents
