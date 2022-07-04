209d27ce69a3b310712390a933bf17ab235cbe74
Ben W <merovex@hey.com>
2022-07-04 14:48:48 -0400

    reverse

--- db/migrate/20220704183259_make_start_time_string.rb
+++ db/migrate/20220704183259_make_start_time_string.rb
@@ -1,5 +1,8 @@
 class MakeStartTimeString < ActiveRecord::Migration[7.0]
-  def change
+  def up
     change_column :reminders, :start_time, :string
+  end
+  def down
+    change_column :reminders, :start_time, :datetime
   end
 end
