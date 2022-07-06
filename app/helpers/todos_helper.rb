ff459b57f0a1c183a8caba0818ee9ce32eb45a6f
Ben W <merovex@hey.com>
2022-07-06 18:48:41 -0400

    style completed todos

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,15 +1,16 @@
 module TodosHelper
   def todo_checkbox(todo)
     if todo.done_at.nil?
-      # data: { turbo_method: :get, turbo_confirm: "Are you sure you want to broadcast?"}
       button_to(
         bi_icon('square'),
         complete_todo_path(todo),
         method: :put
-        # data: { turbo_method: :get, turbo_confirm: "Are you sure you want to broadcast?" }
       )
     else
       bi_icon('check-square-fill', class: 'fill-brand')
     end
+  end
+  def todo_css(todo)
+    (todo.done_at.nil?) ? "" : " completed"
   end
 end
