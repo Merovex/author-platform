a1f804ae036c91770840a8bea891c088b854f550
Ben W <merovex@hey.com>
2022-07-07 04:16:06 -0400

    fix the need to change polymorphic relationship

--- db/migrate/20220707080658_rename_bucket_to_project.rb
+++ db/migrate/20220707080658_rename_bucket_to_project.rb
@@ -2,5 +2,9 @@ class RenameBucketToProject < ActiveRecord::Migration[7.0]
   def change
     rename_table :buckets, :projects
     rename_column :writing_entries, :bucket_id, :project_id
+    Todolist.all.each do |td|
+      td.todolistable_type = 'Project' if td.todolistable_type == 'Bucket'
+      td.save
+    end
   end
 end
