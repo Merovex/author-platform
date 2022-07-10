202f4d60f6949a6c0dbf4c3810f681d53d867685
Ben W <merovex@hey.com>
2022-07-10 17:24:14 -0400

    fixing notifications

--- db/migrate/20220710161647_create_notifications.rb
+++ db/migrate/20220710161647_create_notifications.rb
@@ -1,5 +1,6 @@
 class CreateNotifications < ActiveRecord::Migration[7.0]
   def change
+    drop_table :notifications
     create_table :notifications do |t|
       t.references :recipient, polymorphic: true, null: false
       t.string :type, null: false
